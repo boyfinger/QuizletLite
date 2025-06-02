5a0dbabd0786cee4976d5f4ca4407b3a4e0da75e
dungle2107 <ttd21072004@gmail.com>
2025-06-03 01:09:36 +0700

    add manage user & quiz

---
+++ API/Dtos/User/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Dtos.User
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Username { get; set; } = null!;
+
+        public string? RoleName { get; set; }
+    }
+}
