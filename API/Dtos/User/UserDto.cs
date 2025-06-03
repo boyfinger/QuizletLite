064c1033501072266485277621e0dfd23cb804ec
dungle2107 <ttd21072004@gmail.com>
2025-06-03 08:30:12 +0700

    userDTO

--- API/Dtos/User/UserDto.cs
+++ API/Dtos/User/UserDto.cs
@@ -5,8 +5,7 @@
         public int Id { get; set; }
 
         public int RoleId { get; set; }
-
-        public string Email { get; set; } = null!;
+        public string Avatar { get; set; }
 
         public string Username { get; set; } = null!;
 
