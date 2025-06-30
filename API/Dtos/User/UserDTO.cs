74f139b407f5721c77d64b16b01822ea767d199d
phamquy2602 <144997102+Quoctrung989@users.noreply.github.com>
2025-06-30 23:42:19 +0700

    Update UserDTO.cs

--- API/Dtos/User/UserDTO.cs
+++ API/Dtos/User/UserDTO.cs
@@ -1,13 +1,14 @@
 ﻿namespace API.Dtos.User
 {
-    public class UserDTO
+    public class UserDto
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
+
         public int RoleId { get; set; }
-        public string? RoleName { get; set; } // Nếu cần tên vai trò thay vì ID
         public string Avatar { get; set; }
 
+        public string Username { get; set; } = null!;
+
+        public string? RoleName { get; set; }
     }
 }
